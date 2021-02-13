cd309d809eb9af25e8816cb70172d4cf2877d300
Arpit Bharti <arpitbharti73@gmail.com>
2021-02-13 19:36:11 +0530

    mac settings

--- vsCodeExtension.sh
+++ vsCodeExtension.sh
@@ -23,3 +23,28 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension usernamehw.errorlens
 code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension zhuangtongfa.material-theme
+
+
+
+# MacOS
+
+
+code --install-extension artlaman.chalice-icon-theme
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension enkia.tokyo-night
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension gtwsky.oolory
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension mikestead.dotenv
+code --install-extension naumovs.color-highlight
+code --install-extension PKief.material-icon-theme
+code --install-extension redhat.vscode-yaml
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension usernamehw.errorlens
+code --install-extension VisualStudioExptTeam.vscodeintellicode
